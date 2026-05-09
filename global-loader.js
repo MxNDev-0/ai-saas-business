d206a982530cc8030ca896eb0fb847bb997345de
NetHead <online.survey50000@gmail.com>
2026-05-09 16:16:19 +0100

    Create global-loader.js

---
+++ global-loader.js
@@ -0,0 +1,11 @@
+(function () {
+
+  const script = document.createElement("script");
+
+  script.type = "module";
+
+  script.src = "./maintenance-client.js";
+
+  document.head.appendChild(script);
+
+})();
