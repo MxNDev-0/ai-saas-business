181dda55535f54e8e4a351af4e5dc3dd14508627
NetHead <online.survey50000@gmail.com>
2026-05-13 14:13:06 +0100

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,26 +1,6 @@
-import { db } from "./firebase.js";
-import {
-  collection,
-  query,
-  where,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Notifications system removed permanently
+// MCN Engine is now ads + content platform only
 
-export function listenNotifications(uid, callback) {
-
-  const q = query(
-    collection(db, "events"),
-    where("payload.to", "==", uid)
-  );
-
-  onSnapshot(q, snap => {
-
-    const notes = [];
-
-    snap.forEach(d => notes.push(d.data()));
-
-    callback(notes);
-
-  });
-
+export function listenNotifications() {
+  return [];
 }
