586aed8cf3f9b725249e98dc49c5c9aff5250333
NetHead <online.survey50000@gmail.com>
2026-05-13 14:12:43 +0100

    Update router.js

--- router.js
+++ router.js
@@ -1,17 +1,10 @@
 export function goToPost(id) {
-
   const base = window.location.origin + window.location.pathname.split("/").slice(0, -1).join("/");
-
-  const url = `${base}/post.html?id=${id}`;
-
-  window.location.href = url;
-
+  window.location.href = `${base}/post.html?id=${id}`;
 }
 
 export function safeShare(id) {
-
   const base = window.location.origin + window.location.pathname.split("/").slice(0, -1).join("/");
-
   const url = `${base}/post.html?id=${id}`;
 
   if (navigator.share) {
@@ -20,5 +13,10 @@ export function safeShare(id) {
     navigator.clipboard.writeText(url);
     alert("Link copied");
   }
-
 }
+
+/* SOCIAL FEATURES REMOVED:
+- profile routing
+- messages routing
+- notifications routing
+*/
