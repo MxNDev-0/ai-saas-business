49f705b6a6591e972d937d2eb2ee57f2c43abd22
NetHead <online.survey50000@gmail.com>
2026-05-14 14:38:28 +0100

    Update admin-auth.js

--- admin/admin-auth.js
+++ admin/admin-auth.js
@@ -2,57 +2,23 @@
    MCN ADMIN AUTH
 ========================================= */
 
-import { auth, db } from "./firebase.js";
+import { auth } from "../firebase.js";
 
 import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import {
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+onAuthStateChanged(auth, (user) => {
 
-window.__MCN_ADMIN_AUTH = false;
+  if (!user) {
 
-onAuthStateChanged(auth, async (user) => {
+    console.log("❌ Not logged in");
 
-  try {
+    location.href = "../index.html";
 
-    if (!user) {
-
-      location.href = "./index.html";
-      return;
-    }
-
-    const snap =
-      await getDoc(
-        doc(db, "users", user.uid)
-      );
-
-    if (!snap.exists()) {
-
-      location.href = "./index.html";
-      return;
-    }
-
-    const data = snap.data();
-
-    if (data.role !== "admin") {
-
-      location.href = "./dashboard.html";
-      return;
-    }
-
-    console.log("✅ Admin Auth Passed");
-
-    window.__MCN_ADMIN_AUTH = true;
-
-  } catch (err) {
-
-    console.error("ADMIN AUTH ERROR:", err);
-
-    location.href = "./index.html";
+    return;
   }
+
+  console.log("✅ Admin authenticated");
 
 });
