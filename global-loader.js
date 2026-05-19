b3b3454a1b7ce889e0176fa5f3334edaaecb575f
NetHead <online.survey50000@gmail.com>
2026-05-19 14:18:58 +0100

    Update global-loader.js

--- global-loader.js
+++ global-loader.js
@@ -1,11 +1,9 @@
+// DISABLE THIS TEMPORARILY
 (function () {
-
+  /*
   const script = document.createElement("script");
-
   script.type = "module";
-
   script.src = "./maintenance-client.js";
-
   document.head.appendChild(script);
-
+  */
 })();
